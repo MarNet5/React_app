eb46cda0b5166634ceed091eb63cb31d9e4484e9
Marina Netrebchuk <marinanetrebchuk@MacBook-Air-Marina.local>
2018-10-28 14:17:32 +0200

    Styling login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLoginGoogle, startLoginFacebook }) => (
     <div className="box-layout">
       <div className="box-layout__box">
           <h1 className="box-layout__title">Grandma's kitchen</h1>
-          <p>Where sweet things happen</p>
+          <p className="box-layout__subtitle">Where sweet things happen</p>
         <button className="button" onClick={startLoginGoogle}>Login with Google</button>
         <button className="button" onClick={startLoginFacebook}>Login with Facebook</button>
         {/* <button className="button" onClick={startLoginGithub}>Login with Github</button> */}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,7 +15,7 @@ const config = {
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
   const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
   
-  const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+  
 
 
   export { 
